9ec53b22bc3992122b76fd5d998f877a1c733e19
Knav <me@knav.link>
2021-09-16 05:41:37 -0700

    Adds gaussian folder

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,13 +1,16 @@
 import { EthNetwork, EthNetworkConfig, NETWORK_CONFIG } from "../../shared/config/network"
 
+const ENV_VAR_NET_NAME = "localhost" //process.env.NEXT_PUBLIC_ETH_NETWORK ?? "mainnet"
+console.log(`Current Network: ${ENV_VAR_NET_NAME}`)
+
 export function getCurrentNetwork(): EthNetwork {
-  const envVarNetName = process.env.NEXT_PUBLIC_ETH_NETWORK ?? "mainnet"
-  const ethNetwork: EthNetwork = EthNetwork[envVarNetName.toLowerCase() as EthNetwork]
+  const ethNetwork: EthNetwork = EthNetwork[ENV_VAR_NET_NAME.toLowerCase() as EthNetwork]
   if (!ethNetwork) {
-    throw new Error(`Unrecognized network found: ${envVarNetName}`)
+    throw new Error(`Unrecognized network found: ${ENV_VAR_NET_NAME}`)
   }
   return ethNetwork
 }
+
 
 export function getNetworkConfig(): EthNetworkConfig {
   return NETWORK_CONFIG[getCurrentNetwork()]
