1b9799851740b55fd4f3dab254020f995e228ea4
Knav <me@knav.link>
2021-09-17 14:26:32 -0700

    Adds rarity tier count as metadata

--- api/v1/metadata/[tokenId].ts
+++ api/v1/metadata/[tokenId].ts
@@ -9,6 +9,14 @@ const existsAsync = promisify(exists)
 
 const description = "A set of 8 random numbers whose rarity follows a Gaussian distribution. Generated and stored on-chain using the power of the central limit theorem."
 
+const RARITY_MAPPINGS: Record<string, Array<number>> = {
+  "Commons": [8, 9, 10, 11, 12],
+  "Uncommons": [6, 7, 13, 14],
+  "Rares": [4, 5, 15, 16],
+  "Epics": [2, 3, 17, 18],
+  "Legendaries": [0, 1, 19, 20],
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -70,6 +78,23 @@ export default async function handler(
           "trait_type": "Sum",
           "value": token.numbers.reduce((prev, next) => prev + next, 0).toString(),
         },
+        ...(Object.entries(RARITY_MAPPINGS).map(([label, numbers]: [string, Array<number>]) => {
+          const count = token.numbers.reduce((prev, next) => {
+            if (numbers.includes(next)) {
+              return prev + 1
+            }
+            return prev
+          }, 0)
+
+          if (count === 0) {
+            return
+          }
+
+          return {
+            "trait_type": label,
+            "value": count,
+          }
+        })).filter(n => n),
       ],
     }
 
